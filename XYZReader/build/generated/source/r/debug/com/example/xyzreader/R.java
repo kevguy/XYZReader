c89c08869e107766d11abdec77dfbe88a2bfb32a
kevguy <kevlai22@uw.edu>
2016-06-24 18:47:44 +0800

    fix: fix iamges and texts

--- XYZReader/build/generated/source/r/debug/com/example/xyzreader/R.java
+++ XYZReader/build/generated/source/r/debug/com/example/xyzreader/R.java
@@ -2325,15 +2325,20 @@ containing a value of this type.
         public static final int highlight_alpha_material_colored=0x7f080076;
         public static final int highlight_alpha_material_dark=0x7f080077;
         public static final int highlight_alpha_material_light=0x7f080078;
-        public static final int item_touch_helper_max_drag_scroll_per_frame=0x7f080079;
-        public static final int item_touch_helper_swipe_escape_max_velocity=0x7f08007a;
-        public static final int item_touch_helper_swipe_escape_velocity=0x7f08007b;
+        public static final int item_elevation=0x7f080079;
+        public static final int item_margin=0x7f08007a;
+        public static final int item_touch_helper_max_drag_scroll_per_frame=0x7f08007b;
+        public static final int item_touch_helper_swipe_escape_max_velocity=0x7f08007c;
+        public static final int item_touch_helper_swipe_escape_velocity=0x7f08007d;
         public static final int list_side_margin=0x7f080023;
         public static final int list_toolbar_side_margin=0x7f080024;
-        public static final int notification_large_icon_height=0x7f08007c;
-        public static final int notification_large_icon_width=0x7f08007d;
-        public static final int notification_subtext_size=0x7f08007e;
-        public static final int refresh_indicator_elevation=0x7f08007f;
+        public static final int notification_large_icon_height=0x7f08007e;
+        public static final int notification_large_icon_width=0x7f08007f;
+        public static final int notification_subtext_size=0x7f080080;
+        public static final int refresh_indicator_elevation=0x7f080081;
+        public static final int text_item_padding=0x7f080082;
+        public static final int text_item_small_padding=0x7f080083;
+        public static final int text_margin=0x7f080084;
     }
     public static final class drawable {
         public static final int abc_ab_share_pack_mtrl_alpha=0x7f020000;
